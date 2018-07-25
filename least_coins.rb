d6862bd035d8885c746b956f18084f2cfb373d29
Kyla McClure <kyla.mcclure.16@tfacorps.org>
2018-07-25 18:37:03 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -1,25 +1,36 @@
 
-def least_coins(cents)
-  coin_types = {
-    :quarters => 0,
-    :dimes => 0,
-    :nickels => 0,
-    :pennies => 0
-  }
-  
-  #try to use up all the possible quarters I can 
-  #a quarter is worth 25 cents 
-  #if there is more than 25 cents , use a quarter 
-  #else go to the next one 
-  
-  number_quarters = cents/25
-  coin_types[:quarters] = number_quarters
-  
-  number_dimes = cents/10
-  coin_types[:dimes] = number_dimes
-  
-  puts coin_types
 
-end
+
+
+
+
+
+
+
+
+
+
+# def least_coins(cents)
+#   coin_types = {
+#     :quarters => 0,
+#     :dimes => 0,
+#     :nickels => 0,
+#     :pennies => 0
+#   }
+  
+#   #try to use up all the possible quarters I can 
+#   #a quarter is worth 25 cents 
+#   #if there is more than 25 cents , use a quarter 
+#   #else go to the next one 
+  
+#   number_quarters = cents/25
+#   coin_types[:quarters] = number_quarters
+  
+#   number_dimes = cents/10
+#   coin_types[:dimes] = number_dimes
+  
+#   puts coin_types
+
+# end
 
 
