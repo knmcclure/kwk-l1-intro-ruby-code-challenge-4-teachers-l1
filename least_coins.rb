de07e86643a78871bd5a4aeef586c923874528f6
Kyla McClure <kyla.mcclure.16@tfacorps.org>
2018-07-25 18:49:16 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -4,13 +4,14 @@
 def least_coins(cents)
   solution = {}
   
-  #I need to take coins and figure out first how many quarters I have... 
   num_quarters = cents / 25 
   solution[:quarters]=num_quarters
   
+  puts solution
   
 end
 
+least_coins(50)
 
 
 
